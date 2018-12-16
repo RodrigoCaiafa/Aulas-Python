cf0fdcc337b8a8cae013503128abcd7ac62716d9
Rodrigo Caiafa <biorodrigocaiafa@gmail.com>
2018-12-16 12:21:23 -0200

    Python parte 2 aula 5: ajuste no loop com inclusão de erros e tentativas

--- forca.py
+++ forca.py
@@ -3,20 +3,35 @@ def jogar():
     print("***Bem vindo no jogo da forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
+    tentativas = len(palavra_secreta) + (len(palavra_secreta)/2)
 
     while(not enforcou and not acertou):
         chute = input("Qual a letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index += 1
+
+        if chute in palavra_secreta:
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            print("Errou! {} Tentativas restantes".format(tentativas))
+            tentativas -= 1
+
+        enforcou = tentativas == 0
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+
+
+    if (acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
     print("fim de jogo")
 
